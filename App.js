0c2bf4133639499c9b03d98943f3dca5dffcec7f
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2025-04-01 11:57:05 -0400

    flatlist-intro

--- App.js
+++ App.js
@@ -12,11 +12,12 @@ const data = [
 
 //we can take the array and map them to a flatlist
 //flatlist is a component that takes an array and renders it as a list
-//data is an array of objects that we want to render..we grab that from outside the flatlist comoponent
-//renderItem is a function that renders the data as individual items on our screen
-//we are destructuring the item from the data as individual pieces we are calling "items"
+//data is an array of objects that we want to render..we grab that from outside the flatlist component
+//renderItem is a function that renders the data as individual items on our screen..this is how it will be displayed to the user
+//we are destructuring the item from the data as individual pieces we are calling "item"
 //then using dot notation (.) we can call the individual items properties from the array of objects.
 //item is the object that we are rendering
+//styles.item in the css rule we made in styles.js on how the list item should look..the .item in this case does not come from the list but from the styles.js file.  do not get them confused
 //we are rendering the value of the object
 //
 
