f0e15f441dddd4fb4ebb3c84046852095babfa5f
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2025-04-01 09:49:25 -0400

    intro to lists

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, FlatList } from "react-native";
+import styles from "./styles";
 
+//seeding data or testing with dummy data
+
+const data = [
+  { id: '1', name: 'Item 1' },
+  { id: '2', name: 'Item 2' },
+  { id: '3', name: 'Item 3' },
+  { id: '4', name: 'Item 4' },
+  { id: '5', name: 'Item 5' },
+];
+
+
+
+  //flatlists can take a few different arguments
+  //data: the data you point to that you want to render....usually a data collection/structure like an array or object
+  //renderItem: a function that will return the item you want to render...this it the 'view' part of the data or what the data will look like on screen
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
